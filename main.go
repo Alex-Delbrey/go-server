2eed003999ed6b0c1545768bc97ec2856a4841f5
Jandro <delbreyalex@gmail.com>
2025-04-10 18:42:20 -0400

    basic stuff

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+	serveMux := http.NewServeMux()
+	s := http.Server{
+		Handler: serveMux,
+		Addr:    ":8000",
+	}
+	log.Fatal(http.ListenAndServe(s.Addr, s.Handler))
 	fmt.Println("Hello World")
 }
